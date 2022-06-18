937b1e5a9239db70216bc73593815d9a65bbe791
Viktor Triber <vtriber@ya.ru>
2022-06-18 17:22:02 +0300

    Выполнил условия задания 2

--- oop_task1.py
+++ oop_task1.py
@@ -7,12 +7,30 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
+            if course in lecturer.lectur_grades:
+                lecturer.lectur_grades[course] += [grade]
+            else:
+                lecturer.lectur_grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.lectur_grades = {}
+
+
+class Reviewer(Mentor):
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -24,18 +42,10 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecture(Mentor):
-    pass
+best_student = Student('Ruoy', 'Eman', 'Man')
+best_student.courses_in_progress += ['Python', 'JS']
 
-
-class Reviewer(Mentor):
-    pass
-
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
